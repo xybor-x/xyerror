159bb4436cc6fc14bfcb8e21792767122bfdaa6d
huykingsofm <huykingsofm@gmail.com>
2022-09-01 18:27:49 +0000

    PLF-102 Remove generator, NewClassM, module id in xyerror

--- default.go
+++ default.go
@@ -1,18 +1,15 @@
 package xyerror
 
-// Default is the default error generator.
-var Default = Register("default", 100000)
-
 // Default predefined errors.
 var (
-	Error               = Default.NewClass("Error")
-	IOError             = Default.NewClass("IOError")
-	FloatingPointError  = Default.NewClass("FloatingPointError")
-	IndexError          = Default.NewClass("IndexError")
-	KeyError            = Default.NewClass("KeyError")
-	NotImplementedError = Default.NewClass("NotImplementedError")
-	ValueError          = Default.NewClass("ValueError")
-	ParameterError      = Default.NewClass("ParameterError")
-	TypeError           = Default.NewClass("TypeError")
-	AssertionError      = Default.NewClass("AssertionError")
+	Error               = NewClass("Error")
+	IOError             = NewClass("IOError")
+	FloatingPointError  = NewClass("FloatingPointError")
+	IndexError          = NewClass("IndexError")
+	KeyError            = NewClass("KeyError")
+	NotImplementedError = NewClass("NotImplementedError")
+	ValueError          = NewClass("ValueError")
+	ParameterError      = NewClass("ParameterError")
+	TypeError           = NewClass("TypeError")
+	AssertionError      = NewClass("AssertionError")
 )

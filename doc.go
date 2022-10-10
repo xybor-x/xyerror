f480ab78d66cdfe356dfd2d58fdd57ace32b2ba2
huykingsofm <huykingsofm@gmail.com>
2022-10-10 14:07:32 +0000

    Update version 1.0.0

--- doc.go
+++ doc.go
@@ -18,19 +18,5 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-package xyerror_test
-
-import (
-	"strings"
-	"testing"
-
-	"github.com/xybor-x/xycond"
-	"github.com/xybor-x/xyerror"
-)
-
-func TestException(t *testing.T) {
-	var c1 = xyerror.NewException("class1")
-	var c2 = c1.NewException("class2")
-	xycond.ExpectTrue(strings.Contains(c1.Error(), "class1")).Test(t)
-	xycond.ExpectTrue(strings.Contains(c2.Error(), "class2")).Test(t)
-}
+// Package xyerror supports to define and compare errors conveniently.
+package xyerror
